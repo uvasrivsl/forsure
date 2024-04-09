6ddd511da0fb9452bb2b96cabb9bda5649d8b082
Uvasri .E <uvasri.vsltech@gmail.com>
2024-04-09 10:12:04 +0530

    UPDATE : Contact us and Profile screen added

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,10 +1,79 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Text, TouchableOpacity, Linking, StyleSheet } from 'react-native';
+
+const contacts = [
+  {
+    name: 'Perumal',
+    phone: '9445520417',
+    email: 'sp@fego.in',
+  },
+  {
+    name: 'Praveen',
+    phone: '8098912828',
+    email: 'praveen@fego.in',
+  },
+  {
+    name: 'Kothanda Raman',
+    phone: '9962584838',
+    email: 'tsperumal@gmail.com',
+  },
+];
 
 export default function ContactUs() {
   return (
-    <View>
-      <Text>ContactUs</Text>
+    <View style={styles.container}>
+      {contacts.map((contact, index) => (
+        <View key={index} style={styles.card}>
+          <View style={styles.contactInfo}>
+            <Text style={styles.label}>Name:</Text>
+            <Text style={styles.value}>{contact.name}</Text>
+          </View>
+          <View style={styles.contactInfo}>
+            <Text style={styles.label}>Phone:</Text>
+            <TouchableOpacity onPress={() => Linking.openURL(`tel:${contact.phone}`)}>
+              <Text style={[styles.value, styles.link]}>{contact.phone}</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.contactInfo}>
+            <Text style={styles.label}>Email:</Text>
+            <TouchableOpacity onPress={() => Linking.openURL(`mailto:${contact.email}`)}>
+              <Text style={[styles.value, styles.link]}>{contact.email}</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      ))}
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    // flex: 1,
+    display:'flex',
+    padding: 10,
+    // backgroundColor: '#fff',
+  },
+  card: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    marginBottom: 10,
+    padding: 10,
+    elevation: 10,
+  },
+  contactInfo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 5,
+  },
+  label: {
+    fontWeight: 'bold',
+    marginRight: 5,
+  },
+  value: {
+    flex: 1,
+  },
+  link: {
+    color: 'blue',
+    textDecorationLine: 'underline',
+  },
+});
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,34 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Dimensions } from 'react-native'
 import React from 'react'
-
+const windowWidth = Dimensions.get('window').width;
 export default function Profile() {
   return (
-    <View>
-      <Text>Profile</Text>
+    <View style={styles.container} >
+        <View style={styles.card} >
+        <Text>Profile</Text>
+        </View>
+     
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 20,
+    },
+    card: {
+      backgroundColor: '#fff',
+      borderRadius: 10,
+      overflow: 'hidden',
+      marginBottom: 20,
+      elevation: 10,
+      padding:10,
+      width: windowWidth/1.1,
+      minHeight: 120, 
+ 
+    },
+  });
+  
